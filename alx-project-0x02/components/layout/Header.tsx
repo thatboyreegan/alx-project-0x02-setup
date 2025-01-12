b9c44848472e495ff55bc973cb19c4f77c81513e
thatboyreegan <reaganronald491@gmail.com>
2025-01-12 15:59:38 +0300

    fix: swapped the a tag for the Link tag

--- alx-project-0x02/components/layout/Header.tsx
+++ alx-project-0x02/components/layout/Header.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import Link from 'next/link';
 
 const Header = () => {
   return (
     <div className='grid grid-cols-3 gap-4'>
-      <a href="/home">Home</a>
-      <a href="/about">About Us</a>
-      <a href="/posts">Posts</a>
+      <Link href="/home">Home</Link>
+      <Link href="/about">About Us</Link>
+      <Link href="/posts">Posts</Link>
     </div>
   );
 }
